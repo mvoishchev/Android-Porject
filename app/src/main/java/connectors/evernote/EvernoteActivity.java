e918ee39b81b0547ffe301b2717ba679262cd952
JurisPuchin <jurispuchin@gmail.com>
2015-10-14 03:16:36 -0700

    created basic UI for evernote testing

--- app/src/main/java/connectors/evernote/EvernoteActivity.java
+++ app/src/main/java/connectors/evernote/EvernoteActivity.java
@@ -2,31 +2,39 @@ package connectors.evernote;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import t4.csc413.smartchef.R;
 
 public class EvernoteActivity extends AppCompatActivity {
 
+    String ingredientList = "";
+    TextView ingredientView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_evernote);
-    }
 
-    EditText editText = (EditText) findViewById(R.id.add_ingredient_field);
+        final EditText editText = (EditText) findViewById(R.id.add_ingredient_field);
+        ingredientView = (TextView) findViewById(R.id.evernote_list_of_ingredients);
 
-    editText.setOnEditorActionListener(new OnEditorActionListener() {
-        @Override
-        public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-            boolean handled = false;
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
 
-            if (actionId == EditorInfo.IME_ACTION_SEND) {
-                handled = true;
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+
+                if (ingredientList.equals(""))
+                    ingredientList = editText.getText().toString();
+                else
+                    ingredientList = ingredientList + "\n" + editText.getText().toString();
+
+                ingredientView.setText(ingredientList);
+                return true;
             }
-            return true;
-        }
-    });
-
+        });
+    }
 }
