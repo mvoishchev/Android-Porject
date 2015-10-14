3b0f999d9daa4cc2a3a29e0f6eec3d15b3e92656
JurisPuchin <jurispuchin@gmail.com>
2015-10-14 02:18:59 -0700

    working on evernote input activity

--- app/src/main/java/connectors/evernote/EvernoteActivity.java
+++ app/src/main/java/connectors/evernote/EvernoteActivity.java
@@ -2,6 +2,8 @@ package connectors.evernote;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
 
 import t4.csc413.smartchef.R;
 
@@ -12,4 +14,19 @@ public class EvernoteActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_evernote);
     }
+
+    EditText editText = (EditText) findViewById(R.id.add_ingredient_field);
+
+    editText.setOnEditorActionListener(new OnEditorActionListener() {
+        @Override
+        public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+            boolean handled = false;
+
+            if (actionId == EditorInfo.IME_ACTION_SEND) {
+                handled = true;
+            }
+            return true;
+        }
+    });
+
 }
