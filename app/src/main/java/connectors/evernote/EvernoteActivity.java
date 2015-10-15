5b2199808a0628b69b1d40dc68bfbde01ed3af9d
JurisPuchin <jurispuchin@gmail.com>
2015-10-14 19:07:43 -0700

    Added upload feature to evernote

--- app/src/main/java/connectors/evernote/EvernoteActivity.java
+++ app/src/main/java/connectors/evernote/EvernoteActivity.java
@@ -1,12 +1,20 @@
 package connectors.evernote;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.evernote.client.android.EvernoteSession;
+import com.evernote.client.android.EvernoteUtil;
+import com.evernote.client.android.asyncclient.EvernoteCallback;
+import com.evernote.client.android.asyncclient.EvernoteNoteStoreClient;
+import com.evernote.edam.type.Note;
 
 import t4.csc413.smartchef.R;
 
@@ -15,10 +23,37 @@ public class EvernoteActivity extends AppCompatActivity {
     String ingredientList = "";
     TextView ingredientView;
     EvernoteSession mEvernoteSession;
+    private Intent mCachedIntent;
 
     private static final String CONSUMER_KEY = "jurispuchin";
     private static final String CONSUMER_SECRET = "429acb859dbe6ecb";
     private static final EvernoteSession.EvernoteService EVERNOTE_SERVICE = EvernoteSession.EvernoteService.SANDBOX;
+
+    public void uploadButtonClick(View view) {
+
+        if (!EvernoteSession.getInstance().isLoggedIn()) {
+            return;
+        }
+
+        EvernoteNoteStoreClient noteStoreClient = EvernoteSession.getInstance().getEvernoteClientFactory().getNoteStoreClient();
+
+        Note note = new Note();
+        note.setTitle("SmartChef Shopping List");
+        note.setContent(EvernoteUtil.NOTE_PREFIX + ingredientList + EvernoteUtil.NOTE_SUFFIX);
+
+        noteStoreClient.createNoteAsync(note, new EvernoteCallback<Note>() {
+            @Override
+            public void onSuccess(Note result) {
+                ingredientView.setText("Done!");
+                ingredientList = "";
+            }
+
+            @Override
+            public void onException(Exception exception) {
+                Log.e("EVERNOTE LOG", "Error creating note", exception);
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +63,15 @@ public class EvernoteActivity extends AppCompatActivity {
 
         mEvernoteSession = new EvernoteSession.Builder(this)
                 .setEvernoteService(EVERNOTE_SERVICE)
-                .setSupportAppLinkedNotebooks(true)
+                .setSupportAppLinkedNotebooks(false)
                 .build(CONSUMER_KEY, CONSUMER_SECRET)
                 .asSingleton();
+
+        if (!EvernoteSession.getInstance().isLoggedIn()) {
+            // Check if logged in
+            mCachedIntent = this.getIntent();
+            LoginActivity.launch(this);
+        }
 
         final EditText editText = (EditText) findViewById(R.id.add_ingredient_field);
         ingredientView = (TextView) findViewById(R.id.evernote_list_of_ingredients);
