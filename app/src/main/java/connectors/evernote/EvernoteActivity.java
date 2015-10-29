cf644a4b4342a361bf23c0001ea9817fe9e45c7b
JurisPuchin <jurispuchin@gmail.com>
2015-10-29 01:23:15 -0700

    Created main update feature Added update and upload functions for external use

--- app/src/main/java/connectors/evernote/EvernoteActivity.java
+++ app/src/main/java/connectors/evernote/EvernoteActivity.java
@@ -1,6 +1,7 @@
 package connectors.evernote;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -23,23 +24,135 @@ import com.evernote.thrift.TException;
 
 import t4.csc413.smartchef.R;
 
+/**
+ * This class describes the Evernote activity used to setup Evernote settings.
+ */
 public class EvernoteActivity extends AppCompatActivity {
 
+    //Setup class variables
     String ingredientList = "";
     TextView ingredientView;
     EvernoteSession mEvernoteSession;
     Note note;
+    String mainListGuid;
+    boolean previousNoteExists;
     private Intent mCachedIntent;
 
+    //Setup evernote variables
     private static final String CONSUMER_KEY = "jurispuchin";
     private static final String CONSUMER_SECRET = "429acb859dbe6ecb";
     private static final EvernoteSession.EvernoteService EVERNOTE_SERVICE = EvernoteSession.EvernoteService.SANDBOX;
+    public static final String EVERNOTE_PREF = "EvernotePreferencesFile";
+    private static final String MAIN_LIST_NAME = "SmartChef Main Shopping List";
 
+    public void createNewShoppingList(String listName, String listContent) {
+
+        //Login and setup
+        mEvernoteSession = new EvernoteSession.Builder(this)
+                .setEvernoteService(EVERNOTE_SERVICE)
+                .setSupportAppLinkedNotebooks(false)
+                .build(CONSUMER_KEY, CONSUMER_SECRET)
+                .asSingleton();
+
+        if (!EvernoteSession.getInstance().isLoggedIn()) {
+            // Check if logged in
+            mCachedIntent = this.getIntent();
+            LoginActivity.launch(this);
+        }
+
+        //Make the new note
+        Note newList = new Note();
+        newList.setTitle(listName);
+        newList.setContent(listContent);
+
+        //Upload the note
+        if (!EvernoteSession.getInstance().isLoggedIn()) {
+            return;
+        }
+
+        final EvernoteNoteStoreClient noteStoreClient = EvernoteSession.getInstance().getEvernoteClientFactory().getNoteStoreClient();
+
+        noteStoreClient.createNoteAsync(note, new EvernoteCallback<Note>() {
+            @Override
+            public void onSuccess(Note result) {
+                ingredientView.setText("Done!");
+                ingredientList = "";
+            }
+
+            @Override
+            public void onException(Exception exception) {
+                Log.e("Update Log", "Error updating note", exception);
+            }
+        });
+    }
+
+    public void updateMainShoppingList(String listContent) {
+
+        //Login and setup
+        SharedPreferences settings = getSharedPreferences(EVERNOTE_PREF, 0);
+        mainListGuid = settings.getString("mainListGuid", null);
+        previousNoteExists = settings.getBoolean("listExists", false);
+
+        mEvernoteSession = new EvernoteSession.Builder(this)
+                .setEvernoteService(EVERNOTE_SERVICE)
+                .setSupportAppLinkedNotebooks(false)
+                .build(CONSUMER_KEY, CONSUMER_SECRET)
+                .asSingleton();
+
+        if (!EvernoteSession.getInstance().isLoggedIn()) {
+            // Check if logged in
+            mCachedIntent = this.getIntent();
+            LoginActivity.launch(this);
+        }
+
+        //Make the new note
+        final Note newList = new Note();
+        newList.setTitle(MAIN_LIST_NAME);
+        newList.setContent(listContent);
+
+        //Upload the note
+        if (!EvernoteSession.getInstance().isLoggedIn()) {
+            return;
+        }
+
+        final EvernoteNoteStoreClient noteStoreClient = EvernoteSession.getInstance().getEvernoteClientFactory().getNoteStoreClient();
+
+        noteStoreClient.createNoteAsync(note, new EvernoteCallback<Note>() {
+            @Override
+            public void onSuccess(Note result) {
+                mainListGuid = result.getGuid();
+                previousNoteExists = true;
+
+                // We need an Editor object to make preference changes.
+                SharedPreferences settings = getSharedPreferences(EVERNOTE_PREF, 0);
+                SharedPreferences.Editor editor = settings.edit();
+                editor.putString("mainListGuid", mainListGuid);
+                editor.putBoolean("listExists", previousNoteExists);
+
+                // Commit the edits!
+                editor.commit();
+
+                ingredientView.setText("Done!");
+                ingredientList = "";
+            }
+
+            @Override
+            public void onException(Exception exception) {
+                Log.e("Update Log", "Error updating note", exception);
+            }
+        });
+    }
+
+    /**
+     * This function is clalled when the upload button is clicked.
+     *
+     * It uploads a new shopping list.
+     * @param view  Passed from the button click.
+     */
     public void uploadButtonClick(View view) {
 
         note = new Note();
-        note.setGuid("smartchef");
-        note.setTitle("SmartChef Shopping List");
+        note.setTitle("Temp Shopping List");
         note.setContent(EvernoteUtil.NOTE_PREFIX + ingredientList + EvernoteUtil.NOTE_SUFFIX);
 
         if (!EvernoteSession.getInstance().isLoggedIn()) {
@@ -84,11 +197,64 @@ public class EvernoteActivity extends AppCompatActivity {
         });
     }
 
+    /**
+     * This function runs when the update button is clicked.
+     *
+     * It updates the main shopping list.
+     * @param view Passed from click.
+     */
+    public void updateButtonClick(View view) {
+
+        if (!EvernoteSession.getInstance().isLoggedIn()) {
+            return;
+        }
+
+        final EvernoteNoteStoreClient noteStoreClient = EvernoteSession.getInstance().getEvernoteClientFactory().getNoteStoreClient();
+
+        if (previousNoteExists) {
+            try {
+                noteStoreClient.deleteNote(mainListGuid);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        note = new Note();
+        note.setTitle(MAIN_LIST_NAME);
+        note.setContent(EvernoteUtil.NOTE_PREFIX + ingredientList + EvernoteUtil.NOTE_SUFFIX);
+
+        noteStoreClient.createNoteAsync(note, new EvernoteCallback<Note>() {
+            @Override
+            public void onSuccess(Note result) {
+                note = result;
+                mainListGuid = note.getGuid();
+                previousNoteExists = true;
+
+                ingredientView.setText("Done!");
+                ingredientList = "";
+            }
+
+            @Override
+            public void onException(Exception exception) {
+                Log.e("Update Log", "Error updating note", exception);
+            }
+        });
+
+    }
+
+    /**
+     * Sets up the main activity based on previous saved bundle state.
+     * @param savedInstanceState passed from previous activity.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_evernote);
+
+        SharedPreferences settings = getSharedPreferences(EVERNOTE_PREF, 0);
+        mainListGuid = settings.getString("mainListGuid", null);
+        previousNoteExists = settings.getBoolean("listExists", false);
 
         mEvernoteSession = new EvernoteSession.Builder(this)
                 .setEvernoteService(EVERNOTE_SERVICE)
@@ -119,5 +285,19 @@ public class EvernoteActivity extends AppCompatActivity {
                 return true;
             }
         });
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+
+        // We need an Editor object to make preference changes.
+        SharedPreferences settings = getSharedPreferences(EVERNOTE_PREF, 0);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString("mainListGuid", mainListGuid);
+        editor.putBoolean("listExists", previousNoteExists);
+
+        // Commit the edits!
+        editor.commit();
     }
 }
