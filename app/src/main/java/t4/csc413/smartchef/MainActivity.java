4524a0e7575d54012f02d1e7f474be2e610ac481
Poulomi <poulomid@mai.sfsu.edu>
2015-11-04 21:47:33 -0800

    Fixed Search screen layout

--- app/src/main/java/t4/csc413/smartchef/MainActivity.java
+++ app/src/main/java/t4/csc413/smartchef/MainActivity.java
@@ -13,7 +13,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
-
 import connectors.evernote.LoginActivity;
 import android.widget.Button;
 import android.content.DialogInterface;
